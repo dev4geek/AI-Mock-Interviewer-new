d49e55b2918fbecca5d56f319a53a77149944dfa
Madhu Ranjan Dev <madhuranjandevm143@gmail.com>
2026-05-30 10:41:39 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+question = "What is Machine Learning?"
+
+@app.route('/')
+def home():
+    return render_template(
+        'index.html',
+        question=question
+    )
+
+@app.route('/evaluate', methods=['POST'])
+def evaluate():
+
+    answer = request.form['answer']
+
+    score = len(answer.split())
+
+    if score < 10:
+        feedback = "Answer is too short. Add more details."
+
+    elif score < 30:
+        feedback = "Good answer. Try adding examples."
+
+    else:
+        feedback = "Excellent answer."
+
+    return render_template(
+        'index.html',
+        question=question,
+        feedback=feedback
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
