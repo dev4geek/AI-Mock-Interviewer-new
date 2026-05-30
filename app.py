fc2360e4fca920055011b0eb3e684a455707e843
Madhu Ranjan Dev <madhuranjandevm143@gmail.com>
2026-05-30 15:16:35 +0530

    fix server error

--- app.py
+++ app.py
@@ -3,37 +3,21 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 question = "What is Machine Learning?"
-if __name__ == "__main__":
-    app.run()
 
 @app.route('/')
 def home():
-    return render_template(
-        'index.html',
-        question=question
-    )
+    return render_template('index.html', question=question)
 
 @app.route('/evaluate', methods=['POST'])
 def evaluate():
-
-    answer = request.form['answer']
-
+    answer = request.form.get('answer', '')
     score = len(answer.split())
 
     if score < 10:
         feedback = "Answer is too short. Add more details."
-
     elif score < 30:
         feedback = "Good answer. Try adding examples."
-
     else:
         feedback = "Excellent answer."
 
-    return render_template(
-        'index.html',
-        question=question,
-        feedback=feedback
-    )
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('index.html', question=question, feedback=feedback)
